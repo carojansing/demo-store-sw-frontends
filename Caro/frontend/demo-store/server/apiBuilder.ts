3f5394364f847ec4fbf143cc64d8fa3e1cccb78c
carojansing <c.jansing@basecom.de>
2023-05-08 10:58:12 +0200

    initial

--- Caro/frontend/demo-store/server/apiBuilder.ts
+++
@@ -1,9 +0,0 @@
-import { createInstance } from "@shopware-pwa/api-client";
-const runtimeConfig = useRuntimeConfig();
-
-const apiContext = createInstance({
-  endpoint: runtimeConfig.public.shopware.shopwareEndpoint,
-  accessToken: runtimeConfig.public.shopware.shopwareAccessToken,
-});
-
-export default apiContext;
